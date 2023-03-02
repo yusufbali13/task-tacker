96ef72f6fef2b643a130fc54e330f0588c0ae046
Yusuf <baliyusuf675@gmail.com>
2023-03-02 15:50:53 +0300

    v2

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import Button from "./Button";
+
+const Header = () => {
+  return (
+    <div>
+      <Button />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,11 @@
+import Task from "./Task";
+
+const Tasks = () => {
+  return (
+    <div>
+      <Task />
+    </div>
+  );
+};
+
+export default Tasks;
