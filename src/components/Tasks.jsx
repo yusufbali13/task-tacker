b9d2e96421fcd4992c6316fd395717cdb9f78493
Yusuf <baliyusuf675@gmail.com>
2023-03-03 17:02:18 +0300

    task

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -5,9 +5,9 @@ const Tasks = () => {
     <div className="m-auto">
       <form>
         <p>Task</p>
-        <input class="form-control " type="text" value=""></input>
+        <input class="form-control " type="text"></input>
         <p>Day & Time</p>
-        <input class="form-control" type="datetime-local" value=""></input>
+        <input class="form-control" type="text"></input>
         <input
           className="input-btn rounded-2"
           type="submit"
